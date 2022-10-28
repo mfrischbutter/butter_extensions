abd980f7844da77a39de2f8e8c2f6c95854c1b08
Michael Frischbutter <mail.frischbutter@gmail.com>
2022-10-28 17:19:35 +0200

    feat: add some comments

--- lib/butter_extensions.dart
+++ lib/butter_extensions.dart
@@ -1,9 +1,3 @@
 library butter_extensions;
 
-export 'extensions.dart';
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'widget/padding.dart';
--- lib/widget/padding.dart
+++ lib/widget/padding.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 
 extension ButterWidgetPadding on Widget {
+  /// Adds padding around the widget.
+  /// EdgeInsets.all() is used to create the padding.
   Widget paddingAll(double padding) => Padding(
         padding: EdgeInsets.all(padding),
         child: this,
       );
 
+  /// Adds padding around the widget.
+  /// EdgeInsets.only() is used to create the padding.
   Widget paddingFromLTRB(double l, double t, double r, double b) => Padding(
         padding: EdgeInsets.fromLTRB(l, t, r, b),
         child: this,
       );
 
+  /// Adds padding around the widget.
+  /// EdgeInsets.symmetric() is used to create the padding.
   Widget paddingOnly({
     double top = 0,
     double bottom = 0,
@@ -28,6 +34,8 @@ extension ButterWidgetPadding on Widget {
     );
   }
 
+  /// Adds padding around the widget.
+  /// EdgeInsets.symmetric() is used to create the padding.
   Widget paddingSymmetric({
     double horizontal = 0,
     double vertical = 0,
